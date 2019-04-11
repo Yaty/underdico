7820a1e21b89f978a7d757cf50bc78fc3ac3fd4e
Hugo Da Roit <contact@hdaroit.fr>
2019-04-11 23:53:23 +0200

    Use scrypt to hash passwords

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,4 +1,5 @@
 import * as mongoose from 'mongoose';
+import { scrypt, randomBytes } from 'crypto';
 
 export const UserSchema = new mongoose.Schema({
   username: {
@@ -13,3 +14,53 @@ export const UserSchema = new mongoose.Schema({
 }, {
   timestamps: true,
 });
+
+const SCRYPT_SEPARATOR = '$';
+const SCRYPT_MEMBERS_ENCODING = 'hex';
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  // Make sure not to rehash the password if it is already hashed
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  // Hash the password
+  const scryptLen = 64;
+  const salt = randomBytes(scryptLen);
+
+  scrypt(user.password, salt, scryptLen, (err, derivedKey) => {
+    if (err) {
+      return next(err);
+    }
+
+    user.password =
+      scryptLen +
+      SCRYPT_SEPARATOR +
+      salt.toString(SCRYPT_MEMBERS_ENCODING) +
+      SCRYPT_SEPARATOR +
+      derivedKey.toString(SCRYPT_MEMBERS_ENCODING);
+
+    next();
+  });
+});
+
+UserSchema.methods.checkPassword = function(
+  password: string,
+): Promise<boolean> {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    const hashedPassword = user.password as string;
+    const [scryptLen, salt, expectedDerivedKey] = hashedPassword.split(SCRYPT_SEPARATOR);
+
+    scrypt(password, salt, Number(scryptLen), (err, derivedKey) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(expectedDerivedKey === derivedKey.toString(SCRYPT_MEMBERS_ENCODING));
+    });
+  });
+};
