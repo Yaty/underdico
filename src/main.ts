d104f3cd317323873c1ac23ffffec2a5d6be0838
Hugo Da Roit <contact@hdaroit.fr>
2019-06-05 15:35:35 +0200

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { configure } from './app.configuration';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   configure(app);
+  app.enableCors();
   await app.listen(AppModule.port);
 }
 
